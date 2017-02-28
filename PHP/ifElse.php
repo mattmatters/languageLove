c1f623c53babef46d6056bdf572c1e97be83ef59
Matt Lewis <domattthings@gmail.com>
2017-02-28 21:46:31 +0000

    PHP If Else statements

---
+++ PHP/ifElse.php
@@ -0,0 +1,23 @@
+<?php
+
+// Features standard branching for if else statements
+function testIfTrue1($a) {
+    if ($a) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Casts $a as a boolean and returns result
+function testIfTrue2($a) {
+    return (bool) $a;
+}
+
+// Note: var_export() converts result to string
+echo "testIfTrue1(0) will return ".var_export(testIfTrue1(0), true)."\n";
+echo "testIfTrue1(1) will return ".var_export(testIfTrue1(1), true)."\n";
+echo "testIfTrue2(0) will return ".var_export(testIfTrue1(0), true)."\n";
+echo "testIfTrue2(1) will return ".var_export(testIfTrue1(1), true)."\n";
+
+?>

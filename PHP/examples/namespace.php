4ccc4b655a6f61f326adf3b7994b16ee706b7455
Matt Lewis <domattthings@gmail.com>
2017-03-01 02:09:39 +0000

    Formatted style

--- PHP/examples/namespace.php
+++ PHP/examples/namespace.php
@@ -6,7 +6,7 @@ namespace languageLove;
 // a \ before a class or function tells the compiler to refer to a
 // function in the global namespace
 
-function fopen(){
+function fopen() {
     $file = \fopen();    // Our function name is the same as an internal function.
                          // Execute the function from the global space by adding '\'.
 }
